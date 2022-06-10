3cf6e121de93b5b15823b1598f57903092d61444
tdhood <taylordannhood@gmail.com>
2022-06-10 09:07:47 -0700

    added sessions

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 
 
 @app.get('/')
@@ -17,22 +15,27 @@ def display_instructions():
     """Displays survey instructions and start button"""
     title = survey.title
     instructions = survey.instructions
-
+    
     return render_template("survey_start.html", title=title, instructions=instructions)
 
 @app.post('/begin')
 def start_survey():
     """Redirects to first question"""
 
-    return redirect(f'/questions/{len(responses)}')
+    session["responses"] = []
+
+    return redirect(f'/questions/{len(session["responses"])}')
 
 @app.get('/questions/<num>')
 def show_question(num):
     """Generate survey question"""
-   
-    question_data = survey.questions[int(num)]
+    if len(session["responses"]) == num:
+        question = survey.questions[int(num)]
+        return render_template("question.html", question=question)
+    else:
+        return redirect(f'/questions/{len(session["responses"]) + 1}')
 
-    return render_template("question.html", question_data=question_data)
+    
     
 
 @app.post('/answer')
@@ -40,8 +43,11 @@ def save_answer_and_next_question():
     """capture the answer of question and send user to new question"""
     
     answer = request.form['answer']
+    responses = session["responses"]
     responses.append(answer)
+    session["responses"] = responses
     
+
     if len(responses) < len(survey.questions):
         return redirect(f'/questions/{len(responses)}')
     else:
